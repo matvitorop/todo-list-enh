797e39711ce699e2a5b2bd2ecebd0070c19664da
Матвій Тороп <ipz224_tmd@student.ztu.edu.ua>
2025-01-26 00:23:48 +0200

    Create domain models and db context

---
+++ todo-list-enh.Server/Data/ETLDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using todo_list_enh.Server.Models.Domain;
+
+namespace todo_list_enh.Server.Data
+{
+    public class ETLDbContext : DbContext
+    {
+        public ETLDbContext (DbContextOptions contextOptions) : base (contextOptions)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ todo-list-enh.Server/Models/Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace todo_list_enh.Server.Models.Domain
+{
+    public class User
+    {
+        public required Guid id { get; set; }
+        public required string username { get; set; }
+        public required string email { get; set; }
+        public required string password { get; set; }
+        public DateTime createdAt { get; set; }
+    }
+}
