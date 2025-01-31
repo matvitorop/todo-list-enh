9de1b7cde1deec727e3efb6a71926af065d31b77
MatviiTorop <mattor264@gmail.com>
2025-01-31 16:50:24 +0200

    Update structure of activity repositories, simplify methods

---
+++ todo-list-enh.Server/Repositories/Interfaces/IActivityRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace todo_list_enh.Server.Repositories.Interfaces
+{
+    public interface IActivityRepository<TActivity, TTask, TGoal> : IRepository<TActivity> where TActivity : class
+    {
+        Task<IEnumerable<TTask>> GetAllTasks(int activityId);
+        Task<IEnumerable<TGoal>> GetAllGoals(int activityId);
+        Task<IEnumerable<TTask>> GetNotOverdueTasks(int activityId, DateTime currentTime);
+        Task<bool> DeleteItem<T>(int activityId, int itemId) where T : class;
+        Task<bool> ChangeTaskOrder(int activityId, int taskIdToReplace, int taskIdToRemove);
+        Task<bool> AddTask(int activityId, int taskId);
+        Task<bool> AddGoal(int activityId, int goalId);
+    }
+}
--- todo-list-enh.Server/Repositories/Interfaces/IDayRepository.cs
+++ todo-list-enh.Server/Repositories/Interfaces/IDayRepository.cs
@@ -2,14 +2,5 @@
 
 namespace todo_list_enh.Server.Repositories.Interfaces
 {
-    public interface IDayRepository : IRepository<Day>
-    {
-        Task<IEnumerable<DailyTask>> GetAllDailyTasks(int dayId);
-        Task<IEnumerable<DailyGoal>> GetAllDailyGoals(int dayId);
-        Task<IEnumerable<DailyTask>> GetAllNotOverdueTasks(int dayId);
-        Task<IEnumerable<DailyGoal>> GetAllNotOverdueGoals(int dayId);
-        Task<bool> DeleteDailyTask(int dayId, int taskId);
-        Task<bool> DeleteDailyGoal(int dayId, int taskId);
-        Task<bool> ChangeDailyTaskOrder(int dayId, int taskIdToReplace, int taskIdToRemove);
-    }
+    public interface IDayRepository : IActivityRepository<Day, DailyTask, DailyGoal> { }
 }
