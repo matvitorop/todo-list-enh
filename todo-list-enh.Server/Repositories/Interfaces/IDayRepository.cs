4734f3b09f5bd65181a3ae8ba131cdc4c5210066
MatviiTorop <mattor264@gmail.com>
2025-01-31 02:18:33 +0200

    Create test repository interfaces for daily and week tasks

---
+++ todo-list-enh.Server/Repositories/Interfaces/IDayRepository.cs
@@ -0,0 +1,15 @@
+﻿using todo_list_enh.Server.Models.Domain;
+
+namespace todo_list_enh.Server.Repositories.Interfaces
+{
+    public interface IDayRepository : IRepository<Day>
+    {
+        Task<IEnumerable<DailyTask>> GetAllDailyTasks(int dayId);
+        Task<IEnumerable<DailyGoal>> GetAllDailyGoals(int dayId);
+        Task<IEnumerable<DailyTask>> GetAllNotOverdueTasks(int dayId);
+        Task<IEnumerable<DailyGoal>> GetAllNotOverdueGoals(int dayId);
+        Task<bool> DeleteDailyTask(int dayId, int taskId);
+        Task<bool> DeleteDailyGoal(int dayId, int taskId);
+        Task<bool> ChangeDailyTaskOrder(int dayId, int taskIdToReplace, int taskIdToRemove);
+    }
+}
---
+++ todo-list-enh.Server/Repositories/Interfaces/IWeekRepository.cs
@@ -0,0 +1,15 @@
+﻿using todo_list_enh.Server.Models.Domain;
+
+namespace todo_list_enh.Server.Repositories.Interfaces
+{
+    public interface IWeekRepository : IRepository<Week>
+    {
+        Task<IEnumerable<WeekTask>> GetAllWeekTasks(int weekId);
+        Task<IEnumerable<WeekGoal>> GetAllWeekGoals(int weekId);
+        Task<IEnumerable<WeekTask>> GetAllNotOverdueTasks(int weekId);
+        Task<IEnumerable<WeekGoal>> GetAllNotOverdueGoals(int weekId);
+        Task<bool> DeleteWeekTask(int weekId,int taskId);
+        Task<bool> DeleteWeekGoal(int weekId, int taskId);
+        Task<bool> ChangeWeekTaskOrder(int weekId, int taskIdToReplace, int taskIdToRemove);
+    }
+}
